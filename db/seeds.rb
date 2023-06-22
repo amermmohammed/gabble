e1d8eb8cb5df361c0e7afac8ffe704a6626e9489
amermmohammed <amermalikmohammed@gmail.com>
2023-06-22 21:04:25 +0200

    added users via seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+  { username: "johndoe",
+    email: "johndoe@gmail.com",
+    password: "password",
+    password_confirmation: "password" },
+  { username: "janedoe",
+    email: "janedoe@gmail.com",
+    password: "password",
+    password_confirmation: "password" },
+  { username: "johndoe2",
+    email: "johndoe2@gmail.com",
+    password: "password",
+    password_confirmation: "password" },
+  { username: "janedoe2",
+    email: "janedoe2@gmail.com",
+    password: "password",
+    password_confirmation: "password" },
+  { username: "johndoe3",
+    email: "johndoe3@gmail.com",
+    password: "password",
+    password_confirmation: "password" },
+  { username: "janedoe3",
+    email: "janedoe3@gmail.com",
+    password: "password",
+    password_confirmation: "password" }
+])
+
+users.each do |user|
+  puts "Created user: #{user.username}"
+end
