38df7832d7831086dae53ddd5d0e044579531ba8
amermmohammed <amermalikmohammed@gmail.com>
2023-07-14 22:55:44 +0200

    added retweet functionality

---
+++ app/controllers/retweets_controller.rb
@@ -0,0 +1,18 @@
+class RetweetsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    retweet = current_user.retweets.new(origin_tweet_id: params[:tweet_id])
+    if retweet.save
+      redirect_to user_path(current_user.username), notice: "Retweeted!"
+    else
+      redirect_back fallback_location: root_path, alert: "Retweet failed!"
+    end
+  end
+
+  def destroy
+    retweet = current_user.retweets.find(params[:id])
+    retweet.destroy
+    redirect_to user_path(current_user.username), notice: "Retweet removed!"
+  end
+
+end
