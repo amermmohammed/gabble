9255dce5a165203a37a4332db57d4b2689a70559
amermmohammed <amermalikmohammed@gmail.com>
2023-07-14 23:35:49 +0200

    added retweets to activity's

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -3,6 +3,7 @@ class RetweetsController < ApplicationController
   def create
     retweet = current_user.retweets.new(origin_tweet_id: params[:tweet_id])
     if retweet.save
+      retweet.create_activity key: "retweet.created", owner: current_user, recipient: retweet.origin_tweet.user
       redirect_to user_path(current_user.username), notice: "Retweeted!"
     else
       redirect_back fallback_location: root_path, alert: "Retweet failed!"
--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -1,5 +1,6 @@
 class Retweet < ApplicationRecord
+  include PublicActivity::Model
+
   belongs_to :retweeter, class_name: "User"
   belongs_to :origin_tweet, class_name: "Tweet"
-
 end
